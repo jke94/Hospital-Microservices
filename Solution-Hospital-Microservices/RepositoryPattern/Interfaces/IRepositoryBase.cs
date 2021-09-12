2b703efc724cd2005bb079b3b4cedec00d29db40
jke94 <jcarracedo.dev@gmail.com>
2021-09-12 11:27:16 +0200

    Add RepositoryPattern project.

---
+++ Solution-Hospital-Microservices/RepositoryPattern/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿namespace Repository.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindAll();
+        Task<IQueryable<T>> FindAllAsync();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        Task<IQueryable<T>> FindByConditionAsync(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        void Create(IList<T> entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
